f120a5d1f48f959201d3df6f01708843a0cbef4b
jwleeitn <jwleeitn@naver.com>
2021-08-18 16:59:45 +0900

    commit2

--- src/project/test/new1/TC1.java
+++ src/project/test/new1/TC1.java
@@ -3,5 +3,7 @@ package project.test.new1;
 public class TC1 {
     public static void main (String[] args){
         System.out.println("Hello world 1 !!");
+
+        /* 2 */
     }
 }
--- src/project/test/new1/TC2.java
+++ src/project/test/new1/TC2.java
@@ -1,7 +1,28 @@
 package project.test.new1;
 
+import sun.awt.geom.AreaOp;
+
+import javax.xml.soap.SOAPPart;
+import java.io.BufferedReader;
+import java.nio.file.Files;
+
 public class TC2 {
     public static void main (String[] args){
         System.out.println("Hello world 2 !!");
+
+        boolean a;
+
+        String test = null;
+
+        if( "wow".equals(test)){
+            System.out.println("1");
+        }
+        System.out.println("2");
+
+        if( test.equals("wow")){
+            System.out.println("3");
+        }
     }
+
+    /* 2 */
 }
